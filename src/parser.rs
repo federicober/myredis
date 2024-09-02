fcf64cbcb58d1566a1159a8ef3bc6b63d11e3bf7
Federico OBERNDORFER <federico.ober@hotmail.com>
2024-09-02 17:48:28 +0200

    Add support for different value types and more cleaner syntax

--- src/parser.rs
+++ src/parser.rs
@@ -1,42 +1,42 @@
-use std::fmt::Debug;
+use std::iter::once;
+
+#[derive(Debug, PartialEq)]
+pub enum TypedValue {
+    Numeric(f64),
+    Text(String),
+}
+
+impl TypedValue {
+    fn from_str(as_str: &str) -> Self {
+        match as_str.parse::<f64>() {
+            Ok(num) => TypedValue::Numeric(num),
+            Err(_) => TypedValue::Text(String::from(as_str)),
+        }
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Statement {
     // TODO convert this types to &str
-    Set { key: String, value: String },
+    Set { key: String, value: TypedValue },
     Get { key: String },
 }
 
-#[derive(Debug, PartialEq)]
-pub enum ParseError {
-    EmptyStatement,
-    InvalidStatement(String),
-    UnknownCommand,
-}
+pub fn parse(text: &str) -> Result<Statement, &str> {
+    let mut token_iter = text.split_whitespace();
 
-fn fmt_invalid_number_of_args_error(expected_n_args: u8) -> ParseError {
-    ParseError::InvalidStatement(format!("Expecting {} arguments", expected_n_args).to_string())
-}
+    let Some(first_token) = token_iter.next().and_then(|token| Some(token.to_lowercase()) ) else { return Err("Empty expression");};
+    let tokenized: Vec<&str> = once(first_token.as_str()).chain(token_iter).collect();
 
-pub fn parse(text: &str) -> Result<Statement, ParseError> {
-    let token_iter = &mut text.split_whitespace();
-    let Some(first_token) = token_iter.next() else { return Err(ParseError::EmptyStatement);};
-
-    match first_token.to_lowercase().as_str() {
-        "set" => {
-            let [key, value] = token_iter.collect::<Vec<&str>>()[..] else { return Err(fmt_invalid_number_of_args_error(2)); };
-            Ok(Statement::Set {
-                key: key.to_string(),
-                value: value.to_string(),
-            })
-        }
-        "get" => {
-            let [key] = token_iter.collect::<Vec<&str>>()[..] else { return Err(fmt_invalid_number_of_args_error(1)); };
-            Ok(Statement::Get {
-                key: key.to_string(),
-            })
-        }
-        _ => Err(ParseError::UnknownCommand),
+    match tokenized[..] {
+        ["set", key, value] => Ok(Statement::Set {
+            key: key.to_string(),
+            value: TypedValue::from_str(value),
+        }),
+        ["get", key] => Ok(Statement::Get {
+            key: key.to_string(),
+        }),
+        _ => Err("Unkwon command or invalid number of arguments"),
     }
 }
 
@@ -49,7 +49,7 @@ mod tests {
         assert_eq!(
             Ok(Statement::Set {
                 key: String::from("foo"),
-                value: String::from("bar"),
+                value: TypedValue::Text(String::from("bar")),
             }),
             parse("SET foo bar")
         );
@@ -59,9 +59,19 @@ mod tests {
         assert_eq!(
             Ok(Statement::Set {
                 key: String::from("spam"),
-                value: String::from("eggs")
+                value: TypedValue::Text(String::from("eggs"))
             }),
             parse("SET spam eggs")
+        );
+    }
+    #[test]
+    fn set_value_is_correctl_parsed_as_numeric() {
+        assert_eq!(
+            Ok(Statement::Set {
+                key: String::from("foo"),
+                value: TypedValue::Numeric(1.0)
+            }),
+            parse("SET foo 1")
         );
     }
     // TODO make this test work
@@ -73,6 +83,17 @@ mod tests {
     //             value: String::from("goodbye universe")
     //         }),
     //         parse("SET \"hello world\" \"goodbye universe\"")
+    //     );
+    // }
+    // TODO make this test work
+    // #[test]
+    // fn set_value_wrapped_in_quotes_is_read_as_string() {
+    //     assert_eq!(
+    //         Ok(Statement::Set {
+    //             key: String::from("foo"),
+    //             value: String::from("1")
+    //         }),
+    //         parse("SET foo \"1\"")
     //     );
     // }
     // TODO make this test work
@@ -90,7 +111,7 @@ mod tests {
         assert_eq!(
             Ok(Statement::Set {
                 key: String::from("foo"),
-                value: String::from("bar"),
+                value: TypedValue::Text(String::from("bar")),
             }),
             parse("Set foo bar")
         );
@@ -100,7 +121,7 @@ mod tests {
         assert_eq!(
             Ok(Statement::Set {
                 key: String::from("foo"),
-                value: String::from("bar"),
+                value: TypedValue::Text(String::from("bar")),
             }),
             parse(" set foo bar")
         );
@@ -110,31 +131,22 @@ mod tests {
         assert_eq!(
             Ok(Statement::Set {
                 key: String::from("foo"),
-                value: String::from("bar"),
+                value: TypedValue::Text(String::from("bar")),
             }),
             parse("set foo bar ")
         );
     }
     #[test]
     fn set_no_args_returns_invalid_err() {
-        match parse("set") {
-            Err(ParseError::InvalidStatement(_)) => 1,
-            _ => panic!("Did not raise error"),
-        };
+        parse("set").expect_err("Should error");
     }
     #[test]
     fn set_only_one_arg_returns_invalid_err() {
-        match parse("set foo") {
-            Err(ParseError::InvalidStatement(_)) => 1,
-            _ => panic!("Did not raise error"),
-        };
+        parse("set foo").expect_err("Should error");
     }
     #[test]
     fn set_more_than_2_args_returns_invalid_err() {
-        match parse("set foo") {
-            Err(ParseError::InvalidStatement(_)) => 1,
-            _ => panic!("Did not raise error"),
-        };
+        parse("set foo").expect_err("Should error");
     }
     #[test]
     fn simple_get_foo_bar() {
@@ -156,6 +168,6 @@ mod tests {
     }
     #[test]
     fn empty_string_returns_empty_statement() {
-        assert_eq!(Result::Err(ParseError::EmptyStatement), parse(&("")));
+        assert_eq!(Result::Err("Empty expression"), parse(&("")));
     }
 }
